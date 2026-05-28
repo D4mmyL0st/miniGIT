333cf954009f1973c4efe68579b23ae1cec673d7
D4mmyL0st <BringMyFly375@gmail.com>
2026-05-28 21:52:18 +0700

    Добавить вывод истории коммитов. Реализована функция print_history, позволяющая просматривать цепочку коммитов от текущего до начального.

--- core/repo.c
+++ core/repo.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../headers/repo.h"
+#include "../headers/repo.h"
 
 Commit* init_repo(){
 	Commit* c = malloc(sizeof(Commit));
@@ -12,3 +13,12 @@ Commit* init_repo(){
 	return c;
 }
 
+void print_history(Commit* commit) {
+    Commit* cur = commit;
+
+    while (cur) {
+        printf("Commit: %s\n", cur->message);
+        printf("--------\n");
+        cur = cur->parent;
+    }
+}:
