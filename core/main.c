b02f1890cb4dbfe5818b46b1f65598cb758f3109
D4mmyL0st <BringMyFly375@gmail.com>
2026-05-28 20:51:33 +0700

    Добавлен простой тест в main.c, демонстрирующий работу init_repo и add_file. Создаются последовательные версии коммитов с добавлением файлов в репозиторий.

--- core/main.c
+++ core/main.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
 #include "../headers/repo.h"
+#include "../headers/file.h"
 
 int main() {
+    Commit* repo = init_repo();
+    Commit* v1 = add_file(repo, "a.txt", "hello");
+    Commit* v2 = add_file(v1, "b.txt", "world");
+
+    printf("v2 created\n");
+    
     return 0;
 }
+
